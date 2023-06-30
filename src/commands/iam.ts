d7af01f78af9bce6203332d70665b72c978028ab
Lucas Santos <lhs.santoss@gmail.com>
2023-07-01 00:42:01 +0200

    fix: I am command
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/commands/iam.ts
+++ src/commands/iam.ts
@@ -1,43 +1,38 @@
 import { BotContext, TelegramUser } from '../bot.ts'
 import { CommandContext } from '../deps.ts'
+import { toCharName } from '../helpers/playerHelpers.ts'
 import { userObjectFromMessage } from '../helpers/userObjectFromMessage.ts'
 
 export async function iamCommand(ctx: CommandContext<BotContext>) {
 	if (!ctx.message?.text || !ctx.message?.from.id) return
 
-	const characterName = /\/iam (\w*)/gmi.exec(ctx.message.text)?.[1]?.trim()
+	const characterName = /\/iam (.*)/gmi.exec(ctx.message.text)?.[1]?.trim()
 	if (!characterName) {
 		return ctx.reply('Invalid character name, specify a name after /iam')
 	}
 
-	const player = ctx.session.charsToPlayers[characterName]
+	const player = ctx.session.charsToPlayers[toCharName(characterName)]
 	if (player) {
 		await ctx.reply(
-			`Character ${characterName} is already associated with ${player.name}, overwriting...`,
-			{ reply_to_message_id: ctx.message.message_id },
+			`Character *${characterName}* is already associated with *${player.name}*, overwriting...`,
+			{ reply_to_message_id: ctx.message.message_id, parse_mode: 'MarkdownV2' },
 		)
 	}
 
 	const telegramUser: TelegramUser = userObjectFromMessage(ctx)
-	ctx.session.charsToPlayers[characterName] = telegramUser
+	ctx.session.charsToPlayers[toCharName(characterName)] = telegramUser
 
-	const playerCharacters = ctx.session.playersToChars[telegramUser.name]
-	if (!playerCharacters) ctx.session.playersToChars[telegramUser.userId] = [characterName]
-	else if (!playerCharacters.includes(characterName)) playerCharacters.push(characterName)
+	const playerCharacters = ctx.session.playersToChars[telegramUser.userId]
+	if (!playerCharacters) ctx.session.playersToChars[telegramUser.userId] = [toCharName(characterName)]
+	else if (!playerCharacters.includes(toCharName(characterName))) playerCharacters.push(toCharName(characterName))
 
 	await ctx.reply(
-		`Character ${characterName} is now associated with ${
+		`Character *${characterName}* is now associated with ${
 			`@${ctx.message.from.username}` || ctx.message.from.first_name
 		}`,
-		{ reply_to_message_id: ctx.message.message_id },
+		{ reply_to_message_id: ctx.message.message_id, parse_mode: 'MarkdownV2' },
 	)
 
-	await ctx.reply(`This is my character to player mapping: \`\`\`${JSON.stringify(ctx.session.charsToPlayers, null, 2)}\`\`\``, {
-		parse_mode: 'MarkdownV2',
-		reply_to_message_id: ctx.message.message_id,
-	})
-	await ctx.reply(`This is my player to char mapping: \`\`\`${JSON.stringify(ctx.session.playersToChars, null, 2)}\`\`\``, {
-		parse_mode: 'MarkdownV2',
-		reply_to_message_id: ctx.message.message_id,
-	})
+	console.info(`This is my character to player mapping: ${JSON.stringify(ctx.session.charsToPlayers, null, 2)}`)
+	console.info(`This is my player to char mapping: ${JSON.stringify(ctx.session.playersToChars, null, 2)}`)
 }

699722c8fb7d39115bf37fa0b1d078e59210f7df
Lucas Santos <lhs.santoss@gmail.com>
2023-06-30 21:57:42 +0200

    wip: paid balance command
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/handlers/balancePaid.ts
+++ src/handlers/balancePaid.ts
@@ -6,7 +6,17 @@ type ClearBalanceResponseTypes = 'Yes' | 'No' | undefined
 export async function balancePaidHandler (
 	ctx: Filter<BotContext, 'callback_query'>,
 ) {
-	const player = ctx.session.players[ctx.from.id]
+	const chars = ctx.session.playersToChars[ctx.from.id]
+	if (!chars || chars.length === 0) {
+		await ctx.answerCallbackQuery('You are not part of this hunt.')
+		return
+	}
+
+	if (chars.length > 1) {
+		await ctx.answerCallbackQuery(
+			'You have multiple characters registered in your name. Please specify which one you want to mark as paid.',
+		)
+		return
+	}
 	await ctx.answerCallbackQuery('Marking balance as paid...')
-	//TODO: Mark balance as paid
 }

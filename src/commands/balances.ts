044d681072330058af8ea2b136f4b9bba776c43d
Lucas Santos <lhs.santoss@gmail.com>
2023-06-30 21:56:02 +0200

    Add list balances command
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/commands/balances.ts
+++ src/commands/balances.ts
@@ -1,17 +1,29 @@
 import { InlineKeyboard } from '../deps.ts'
 import { BotContext } from '../bot.ts'
+import { makeUserLink } from '../helpers/makeUserLink.ts'
 
 export async function listBalancesCommand (ctx: BotContext) {
-  const balances = Object.entries(ctx.session.balances)
-  const balancesText = balances.length
-    ? balances.map(([player, balance]) => `${player}: ${balance}`).join('\n')
-    : 'No balances yet'
-
+  const players = Object.keys(ctx.session.balances)
   let options = {}
-  if (balancesText !== 'No balances yet, use the /splitloot command to create some') {
+  let balancesText = 'No balances yet, use the /splitloot command to create some'
+
+  if (players.length > 0) {
+    balancesText = players.map((userId) => {
+      const balances = ctx.session.balances[userId]
+      const player = ctx.session.charsToPlayers[userId]
+      const playerName = makeUserLink(player)
+      const balancesText = balances.map(({ from, amount, to }) => {
+        const fromLink = makeUserLink(from)
+        return `- **${fromLink}** owes *${amount}* to **${makeUserLink(to)}**`
+      }).join('\n')
+      return `**${playerName}**:\n${balancesText}`
+    }).join('\n=== === ===\n')
+
     options = {
       reply_markup: (new InlineKeyboard()).text('👍 I paid', 'balance_paid'),
+      parseMode: 'MarkdownV2',
     }
   }
+
   await ctx.reply(balancesText, options)
 }
